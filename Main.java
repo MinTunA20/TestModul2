9b0a0c11bc2df01dfc71de6509f503a2715bd1c0
MinTunA20 <gin.ritani@gmail.com>
2022-06-01 17:27:04 +0700

    add

---
+++ Main.java
@@ -0,0 +1,10 @@
+import Model.ManagerStudent;
+
+public class Main {
+    public static void main(String[] args) {
+        ManagerStudent managerStudent = new ManagerStudent();
+        while (true){
+            managerStudent.menu();
+        }
+    }
+}
